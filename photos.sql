6a09de3fd0d2af47fe527d7ee30c6ac9c8060771
子勰 <code@bihe0832.com>
2023-05-19 17:21:06 +0800

    数据结构完善

--- photos.sql
+++ photos.sql
@@ -23,6 +23,34 @@ CREATE TABLE IF NOT EXISTS tags (
   name VARCHAR(255) NOT NULL -- '标签名称'
 );
 
+
+-- 删除名为 type 的表（如果它已经存在），然后创建一个新的 type 表
+DROP TABLE IF EXISTS type;
+-- 创建名为 type 的数据表
+CREATE TABLE IF NOT EXISTS type (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255) NOT NULL -- '类别名称'
+);
+
+-- 删除名为 theme 的表（如果它已经存在），然后创建一个新的 theme 表
+DROP TABLE IF EXISTS theme;
+-- 创建名为 theme 的数据表
+CREATE TABLE IF NOT EXISTS theme (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255) NOT NULL -- '类别名称'
+);
+
+-- 删除名为 administrative_division 的表（如果它已经存在），然后创建一个新的 administrative_division 表
+DROP TABLE IF EXISTS administrative_division;
+CREATE TABLE IF NOT EXISTS `administrative_division` (
+  `code` TEXT NOT NULL PRIMARY KEY,
+  `name` TEXT NOT NULL,
+  `parent_code` TEXT NOT NULL,
+  `level` INTEGER NOT NULL
+);
+CREATE INDEX IF NOT EXISTS `idx_parent_code` ON `administrative_division` (`parent_code`);
+
+
 -- 删除名为 media 的表（如果它已经存在），然后创建一个新的 media 表
 DROP TABLE IF EXISTS media;
 -- 创建名为 media 的数据表
@@ -32,13 +60,22 @@ CREATE TABLE IF NOT EXISTS media (
   file_md5 VARCHAR(32) NOT NULL, -- '文件md5'
   file_size INTEGER UNSIGNED NOT NULL, -- '文件大小'
   timestamp INTEGER UNSIGNED NOT NULL, -- '时间戳'
-  country VARCHAR(255) NOT NULL, -- '国家'
-  province VARCHAR(255) NOT NULL, -- '省份'
-  city VARCHAR(255) NOT NULL, -- '城市'
+  position VARCHAR(255) NOT NULL, -- '国家'
   time_period_id INTEGER NOT NULL, -- '时间段id'
   duration INTEGER UNSIGNED NOT NULL, -- '时长'
-  FOREIGN KEY (time_period_id) REFERENCES time_period(id)
+  latitude REAL NOT NULL, -- '经度'
+  longitude REAL NOT NULL, -- '纬度'
+  media_width INTEGER, -- '宽度'
+  media_height INTEGER, -- '高度'
+  star INTEGER DEFAULT 1, -- '是否收藏：1 未收藏， 2收藏'
+  review_type INTEGER DEFAULT 1, -- '是否Review：1 未审核， 2已审核'
+  type_id INTEGER NOT NULL, -- '主题'
+  privacy_level INTEGER NOT NULL CHECK (`privacy_level` BETWEEN 1 AND 4), -- '私密等级：1，仅自己可见。2，仅家人可见。3，仅朋友可见。4，所有人可见'
+  FOREIGN KEY (time_period_id) REFERENCES time_period(id),
+  FOREIGN KEY (type_id) REFERENCES type(id),
+  FOREIGN KEY (position) REFERENCES administrative_division(code),
 );
+CREATE INDEX IF NOT EXISTS `idx_timestamp` ON `media`(`timestamp`);
 
 -- 删除名为 media_characters 的表（如果它已经存在），然后创建一个新的 media_characters 表
 DROP TABLE IF EXISTS media_characters;
@@ -62,6 +99,17 @@ CREATE TABLE IF NOT EXISTS media_tags (
   FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
 );
 
+-- 删除名为 media_theme 的表（如果它已经存在），然后创建一个新的 media_theme 表
+DROP TABLE IF EXISTS media_theme;
+-- 创建名为 media_theme 的数据表
+CREATE TABLE IF NOT EXISTS media_theme (
+  media_id INTEGER NOT NULL, -- '媒体资源id',
+  media_theme_id INTEGER NOT NULL, -- '主题id',
+  PRIMARY KEY (media_id, media_theme_id),
+  FOREIGN KEY (media_id) REFERENCES media(id),
+  FOREIGN KEY (media_theme_id) REFERENCES theme(id)
+);
+
 -- 向 time_period 表中插入预定义的时间段记录
 INSERT INTO time_period (period) VALUES 
   ('小时候'),
@@ -70,12 +118,21 @@ INSERT INTO time_period (period) VALUES
   ('初中'),
   ('高中'),
   ('大学'),
-  ('上班');
+  ('爸爸上班'),
+  ('爸爸带娃');
 
 -- 插入预定义的标签记录
+INSERT INTO type (name) VALUES 
+  ('旅行'),
+  ('日常生活'),
+  ('学校日常');
+
+INSERT INTO theme (name) VALUES 
+  ('2019香格里拉'),
+  ('写代码');
+
 INSERT INTO tags (name) VALUES 
-  ('旅游'),
+  ('自然风光'),
   ('美食'),
-  ('运动'),
   ('音乐'),
   ('电影');
